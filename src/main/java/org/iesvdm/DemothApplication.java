5d1f7ccecaf7976df1ed36dc181d6fbb1d35692b
usuario <usuario@DESKTOP-UL3D53O>
2022-12-10 18:47:01 +0100

    Commit inicial

---
+++ src/main/java/org/iesvdm/DemothApplication.java
@@ -0,0 +1,13 @@
+package org.iesvdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemothApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemothApplication.class, args);
+	}
+
+}
